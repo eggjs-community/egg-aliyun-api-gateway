3b5ff6a42c9ef00230b828ec4a55cb82ab8f7076
Thonatos.Yang <thonatos.yang@gmail.com>
2017-07-17 10:14:06 +0800

    test: change aysnc func to generator & add runtime: nodejs-6.x.x

--- test/fixtures/apps/aliyun-api-gateway-test/app/controller/home.js
+++ test/fixtures/apps/aliyun-api-gateway-test/app/controller/home.js
@@ -2,13 +2,11 @@
 
 module.exports = app => {
   return class HomeController extends app.Controller {
-    async index() {
+    * index() {
       const { ctx, app } = this;
       const url = 'https://openapi.insta360.com/community/v1/dailySelection/list?date=2017-07-16&days=1';
       try {
-        const result = await app.aliyunApiGateway.get(url);
-        ctx.status = 200;
-        ctx.body = result;
+        ctx.body = yield app.aliyunApiGateway.get(url);
       } catch (error) {
         ctx.status = 400;
         ctx.body = error.toString();
